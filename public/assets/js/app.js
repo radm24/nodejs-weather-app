b143b02438b49d7ec7a6831a248219995e933604
radm24 <r.musin24@mail.ru>
2021-04-18 22:21:59 +0300

    build settings

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (event) => {
     if (location) {
         loadingEl.classList.remove('hidden');
         
-        fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+        fetch(`/weather?address=${location}`).then(response => {
             response.json().then(data => {
                 loadingEl.classList.add('hidden');
                 if (data.error) {
