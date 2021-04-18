e858bc11c3e3a24cd7a3349a1720d5619ff933c4
radm24 <r.musin24@mail.ru>
2021-04-18 21:42:46 +0300

    Initial commit

---
+++ public/assets/js/app.js
@@ -0,0 +1,50 @@
+const weatherForm = document.querySelector('#weatherForm');
+const weatherInput = document.querySelector('#weatherInput');
+const errorInfo = document.querySelector('#errorInfo');
+const locationInfo = document.querySelector('#locationInfo');
+const forecastInfo = document.querySelector('#forecastInfo');
+const loadingEl = document.querySelector('#loading');
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    hideArrElements([errorInfo, locationInfo, forecastInfo]);
+
+    const location = weatherInput.value;
+    if (location) {
+        loadingEl.classList.remove('hidden');
+        
+        fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+            response.json().then(data => {
+                loadingEl.classList.add('hidden');
+                if (data.error) {
+                    showAndSetTextArrElements([errorInfo], [data.error]);
+                } else {
+                    showAndSetTextArrElements([locationInfo, forecastInfo], [data.location, data.forecast]);
+                }
+            })
+        })
+    }
+})
+
+const hideArrElements = (arr) => {
+    arr.forEach(el => {
+        if (!el.classList.contains('hidden')) {
+            el.classList.add('hidden');
+        }
+    })
+}
+
+const showAndSetTextArrElements = (elArr, textArr, isSameText = 0) => {
+    // if isSameText has falsy value then elArr and text arrays must contain the same number of elements
+    if ((!isSameText && (elArr.length !== textArr.length)) || (!Array.isArray(elArr) || !Array.isArray(textArr))) {
+        console.log('Please, provide correct values for elArr and textArr parameters');
+        return;
+    }
+
+    for (let i = 0; i < elArr.length; i++) {
+        elArr[i].textContent = isSameText ? textArr[0] : textArr[i];
+        if (elArr[i].classList.contains('hidden')) {
+            elArr[i].classList.remove('hidden');
+        }
+    }
+}
---
+++ src/utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('postman-request');
+
+const geocode = (address, callback) => {
+    debugger;
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + process.env.MB_KEY + '&limit=1';
+    request({ url, json: true }, (error, { body }) => {
+        debugger;
+        if (error) {
+            callback('Unable to connect to location services!', undefined);
+        } else if (!body.features.length) {
+            callback('Unable to find location! Try another search.', undefined);
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            });
+        }
+    });
+}
+
+module.exports = geocode;
