4d5d52e7fb95a99efe320d3547fb5625879d6190
radm24 <r.musin24@mail.ru>
2021-05-12 15:00:02 +0300

    removed debugger

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,6 @@
 const request = require('postman-request');
 
 const geocode = (address, callback) => {
-    debugger;
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + process.env.MB_KEY + '&limit=1';
     request({ url, json: true }, (error, { body }) => {
         if (error) {
